f1ef6e7b412e156106c2fcf1269bb44a73375b9f
Mateus de Souza Novaes <mateussn13@hotmail.com>
2020-04-28 20:33:30 -0300

    Update Main.java

--- src/emulator/Main.java
+++ src/emulator/Main.java
@@ -1,43 +1,42 @@
 package emulator;
 
-import chip.Chip;//Importing class Chip from package 'chip'
+import chip.Chip; // Importing class Chip from package 'chip'
 
 public class Main extends Thread {
 	
-	//atributes
+	// atributes
 	private Chip chip8;
 	private ChipFrame frame;
 	
-	//constructor method
+	// constructor method
 	public Main() {
 		chip8 = new Chip();
 		chip8.initialize();
-		chip8.loadProgram("./pong2.c8"); //Loads the ROM
+		chip8.loadProgram("./pong2.c8"); // Loads the ROM
 		frame = new ChipFrame(chip8);
 	}
 	
 	public void run() {
-		//60 Hz == 60 Updates p/second
-		//Emulation Loop
+		// 60 Hz == 60 Updates p/second
+		// Emulation Loop
 		while(true) {
 			chip8.setKeyBuffer(frame.getKeyBuffer());
-			//Emulate just 1 cycle
+			// Emulate just 1 cycle
 			chip8.emulateCycle();
 			
 			// If needsRedraw() return set/true -- flag
-			//The screen will be updated
+			// The screen will be updated
 			if(chip8.needsRedraw()) {
 				frame.repaint();
 				chip8.removeDrawFlag(); 
 			}
 			try {
-				Thread.sleep(1); //1000 / 60 = 16 miliseconds
+				Thread.sleep(1); // 1000 / 60 = 16 miliseconds
 			}catch(InterruptedException e) {
-				//Unthrown exception
+				// Unthrown exception
 			}
 			
 		}
-		
 	}
 	
 	public static void main(String[] args) {
